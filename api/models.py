2fb830557de5fd9b48a36a115d79fc3ce78554dd
ZARIFMAC <uzbzarif@gmail.com>
2022-12-03 18:22:32 +0500

    Quiz model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
