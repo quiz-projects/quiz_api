0f1ccf5bcc27bd88a9490f808d0e669f6ed6069c
ZARIFMAC <uzbzarif@gmail.com>
2022-12-15 13:38:51 +0500

    add student

--- api/models.py
+++ api/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
+class Student(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+    telegram_id = models.IntegerField(unique=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+    
+
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+   
+
+    def __str__(self):
+        return self.title
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
