e455631126cd90ecf0ce30976b6f3363c3e31d6f
sanjarbek <sanjarsaidov173@gmail.com>
2023-01-05 14:10:48 +0500

    url

--- api/urls.py
+++ api/urls.py
@@ -21,8 +21,4 @@ urlpatterns = [
     path('result_detail/', ResultDetailView.as_view()),
     path('result_detail/<int:pk>/', ResultDetailView.as_view()),
     path('updeteStudent/<int:pk>', UpdateStudentView.as_view()),
-    # path('quiz/', QuizListView.as_view()),
-    # path('topic/<int:quiz_id>/', TopicListView.as_view()),
-    # path('question/<int:topic_id>/', QuestionListView.as_view()),
-    # path('option/<int:question_id>/', OptionListView.as_view()),
 ]
